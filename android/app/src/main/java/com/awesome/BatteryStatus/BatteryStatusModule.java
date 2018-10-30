6fd1ced4a050eadf4a577b2779a5c55a475484aa
seka <sekar@onedatasoftware.com>
2018-10-30 16:08:44 +0530

    Working code for get battery status

---
+++ android/app/src/main/java/com/awesome/BatteryStatus/BatteryStatusModule.java
@@ -0,0 +1,36 @@
+package com.awesome.BatteryStatus;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class BatteryStatusModule extends ReactContextBaseJavaModule {
+    public BatteryStatusModule(ReactApplicationContext reactContext) {
+
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "BatteryStatus";
+    }
+
+    @ReactMethod
+    public void getBatteryStatus(Callback successCallback) {
+        Intent batteryIntent = getCurrentActivity().registerReceiver(null,
+                new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+        int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+        if (level == -1 || scale == -1) {
+            level = 0;
+        }
+        String result = Integer.toString((int) (((float) level / (float) scale) * 100.0f));
+        successCallback.invoke(null, result);
+    }
+}
